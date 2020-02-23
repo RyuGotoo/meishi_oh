08641270edf9ccd62f1d89832739619a464052d3
RyuGotoo <ryu.gotoo@gmail.com>
2020-02-23 19:01:19 +0900

    Update meishi_oh.py

--- meishi_oh.py
+++ meishi_oh.py
@@ -2,7 +2,7 @@ import collections
 import requests
 from bs4 import BeautifulSoup
 from janome.tokenizer import Tokenizer
-
+import csv
 
 the_part_of_speech_for_deciding_the_king = '名詞'
 # 名詞, 動詞, 助詞, 副詞, 助動詞, 形容詞, 接続詞, 連体詞, 接頭詞, 感動詞
@@ -31,6 +31,10 @@ def get_urls():
 def format_text(text):
     soup = BeautifulSoup(text, 'html.parser').find(class_='wrapper-article-detail-inner')
     soup.find('dl').decompose()
+    while len(soup.find_all('pre')) > 0:
+        soup.find('pre').decompose()
+    while len(soup.find_all('code')) > 0:
+        soup.find('code').decompose()
     while len(soup.find_all(class_='prettyprint')) > 0:
         soup.find(class_='prettyprint').decompose()
     formatted_text = '\n'.join(soup.stripped_strings)
@@ -52,13 +56,14 @@ def create_blogs(urls):
         pos_list = []
         for token in Tokenizer().tokenize(text):
             pos = token.part_of_speech.split(',')[0]
-            if pos != '記号':
+            base_pos = ('名詞', '助詞', '動詞', '助動詞', '副詞', '形容詞', '連体詞', '接頭詞', '接続詞', '感動詞')
+            if pos in base_pos:
                 pos_list.append(pos)
         blog.pos_num = len(pos_list)
 
         # 品詞 (pos) カウント
-        pos_counts = collections.Counter(pos_list)
-        blog.pos_counts = pos_counts
+        blog.pos_counts = collections.Counter(pos_list)
+        blog.pos_counts = dict(sorted(blog.pos_counts.items(), key=lambda x: -x[1]))
 
         blogs.append(blog)
         print(blog.title)
@@ -69,7 +74,6 @@ def show_fields(blog):
     print(blog.url)
     print(blog.title)
     print('総単語数 :', blog.pos_num)
-    blog.pos_counts = dict(sorted(blog.pos_counts.items(), key=lambda x: -x[1]))
     print(dict(blog.pos_counts))
     pos_rates = blog.get_pos_rates()
     for pos, rate in pos_rates.items():
@@ -82,23 +86,39 @@ def who_is_the_king(pos, blogs):
     # pos率高い順にソート
     ranking = {}
     for blog in blogs:
-        rate = blog.get_pos_rates()[pos]
-        ranking[blog] = rate
+        if pos in blog.get_pos_rates().keys():
+            rate = blog.get_pos_rates()[pos]
+            ranking[blog] = rate
+        else:
+            ranking[blog] = 0
     ranking = dict(sorted(ranking.items(), key=lambda x: -x[1]))
+
     ranked_blogs = []
     for blog in ranking.keys():
         ranked_blogs.append(blog)
 
-    print('【', str(pos) + '率ランキング', '】')
+    print('【 {}率ランキング 】'.format(pos))
     for rank, blog in enumerate(ranked_blogs):
         print(str(rank + 1) + '位')
         show_fields(blog)
+
+
+def export_csv(blogs):
+    with open('results.csv', 'a') as f:
+        base_pos = ['名詞', '助詞', '動詞', '助動詞', '副詞', '形容詞', '連体詞', '接頭詞', '接続詞', '感動詞']
+        header = ['id'] + base_pos
+        writer = csv.DictWriter(f, header)
+        for blog in blogs:
+            pos_rates = blog.get_pos_rates()
+            pos_rates['id'] = blog.url
+            writer.writerow(pos_rates)
 
 
 def main():
     urls = get_urls()
     blogs = create_blogs(urls)
     who_is_the_king(the_part_of_speech_for_deciding_the_king, blogs)
+    export_csv(blogs)
 
 
 if __name__ == '__main__':
